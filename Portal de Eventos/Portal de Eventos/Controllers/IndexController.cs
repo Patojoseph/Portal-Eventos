3d0cb04e14a2e9c79da0779998d1e3081b49b218
José Francisco <josefsp0508@gmail.com>
2025-10-03 13:32:51 -0300

    Criando e testando mais coisas

---
+++ Portal de Eventos/Portal de Eventos/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Portal_de_Eventos.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
