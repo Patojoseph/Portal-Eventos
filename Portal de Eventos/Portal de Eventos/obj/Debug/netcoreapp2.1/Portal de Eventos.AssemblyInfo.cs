54447dbacb15d16c698f174d9d31abc041979fbb
José Francisco <josefsp0508@gmail.com>
2025-10-06 15:07:37 -0300

    cadastro

--- Portal de Eventos/Portal de Eventos/obj/Debug/netcoreapp2.1/Portal de Eventos.AssemblyInfo.cs
+++ Portal de Eventos/Portal de Eventos/obj/Debug/netcoreapp2.1/Portal de Eventos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal de Eventos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e51743e7efb743c381e786fe0b346ce9bc419f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096d1bd37d069b75bcff5414bd546e3f8a40eba8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal de Eventos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal de Eventos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
