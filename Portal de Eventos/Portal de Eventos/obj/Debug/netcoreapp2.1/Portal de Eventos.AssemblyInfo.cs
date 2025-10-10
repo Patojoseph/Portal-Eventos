046086bf27b45a648e636a791264244bb2d6ce25
José Francisco <josefsp0508@gmail.com>
2025-10-10 10:52:12 -0300

    feat: Tela de abrir eventos
    Estrutura inicial da tela de abrir eventos

--- Portal de Eventos/Portal de Eventos/obj/Debug/netcoreapp2.1/Portal de Eventos.AssemblyInfo.cs
+++ Portal de Eventos/Portal de Eventos/obj/Debug/netcoreapp2.1/Portal de Eventos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal de Eventos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c25235e68caa7b5afb785f97c427b4e5f81ccc9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal de Eventos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal de Eventos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
